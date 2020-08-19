cfac3b3ed9a3b956a1f13dbd82b33073c1a31777
Baher-Essam <44957599+Baher-essam@users.noreply.github.com>
2020-08-20 01:43:32 +0200

    adding scrollbar for both screens

--- js/index.js
+++ js/index.js
@@ -7,3 +7,32 @@ window.addEventListener("scroll", function ()
     console.log('offset' + offset);
     parallex.style.backgroundPositionY = offset * 0.6 +"px";
 })
+
+
+
+/*scrollbar width*/
+window.addEventListener("scroll" , event => {
+    let scrollTop =document.documentElement.scrollTop;
+    let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let progressElement = 100 * scrollTop / scrollHeight ;
+    document.querySelector("#progress").style.width = progressElement + "%";
+})
+/*scrollbar height*/
+$(function () {
+    let progressbar = document.getElementById('progressbar');
+    let totalHeight = document.body.scrollHeight - window.innerHeight;
+    window.onscroll = function(){
+        let progressHeight = (window.pageYOffset/totalHeight) *100;
+        progressbar.style.height = progressHeight + '%';
+    }
+})
+
+
+//showNavbar
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar-fixed-top");
+      var $header = $("header")
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $header.height());
+    });
+  });
