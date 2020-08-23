2424bcf63cc2932ca7fc2ff8a4bce612415c8156
Baher-Essam <44957599+Baher-essam@users.noreply.github.com>
2020-08-23 04:01:29 +0200

    remove console log

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ const parallex = document.getElementById("parallex");
 window.addEventListener("scroll", function ()
 {
     let offset = window.pageYOffset;
-    console.log('offset' + offset);
     parallex.style.backgroundPositionY = offset * 0.6 +"px";
 })
 
